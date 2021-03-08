b3379350c2b7ced260cc63b3d817c7c9e520e87f
Андрей <Militarysquirrel2@mail.ru>
2021-03-08 15:05:46 +0300

    An exercise 4

---
+++ Lesson_2/04_student.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# (цикл while)
+
+# Ежемесячная стипендия студента составляет educational_grant руб., а расходы на проживание превышают стипендию
+# и составляют expenses руб. в месяц. Рост цен ежемесячно увеличивает расходы на 3%, кроме первого месяца
+# Составьте программу расчета суммы денег, которую необходимо единовременно попросить у родителей,
+# чтобы можно было прожить учебный год (10 месяцев), используя только эти деньги и стипендию.
+# Формат вывода:
+#   Студенту надо попросить ХХХ.ХХ рублей
+
+educational_grant, expenses = 10000, 12000
+
+# TODO здесь ваш код
+expenses_percent=expenses
+i= 1
+while i <10:
+    i += 1
+    expenses_percent = (expenses_percent/100*3)+expenses_percent
+    print(expenses_percent)
+expenses = expenses*10+expenses_percent
+educational_grant=educational_grant*10
+print('Студенту понадобится еще ', round(expenses - educational_grant, 2), "руб.")
+
+
