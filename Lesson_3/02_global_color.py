34097451c0f0c220740db51a8bd7acef32107b53
Rufus-02 <vertihvost60@gmail.com>
2021-04-01 19:28:19 +0300

    DZ 3.2

--- Lesson_3/02_global_color.py
+++ Lesson_3/02_global_color.py
@@ -16,4 +16,49 @@ import simple_draw as sd
 
 # TODO здесь ваш код
 
+color_list = ['RED', 'ORANGE', 'YELLOW', 'GREEN', 'CYAN', 'BLUE', 'PURPLE']
+color_list_const = [sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN, sd.COLOR_CYAN, sd.COLOR_BLUE,
+                    sd.COLOR_PURPLE]
+for x, y in enumerate(color_list, start=1):
+    print(f'{x} : {y}')
+
+_input = -3
+while 0 > _input or _input > 6:
+    _input = int(input('Введите номер цвета: ')) - 1
+    if -1 < _input < 7:
+        break
+    print("Вы ввели некорректный номер цвета!")
+
+
+def vector(vector_start, length, angle):
+    v = sd.get_vector(vector_start, angle, length)
+    return v.end_point
+
+
+def polygon(point, heads, length, color):
+    angle = 0
+    angle_start = 15
+    angle_polygon = 360 / heads
+    point_polygon = point
+    for _ in range(heads):
+        if _ == 0:
+            angle = angle_start
+        else:
+            angle += angle_polygon
+        if _ < (heads - 1):
+            end_point = vector(point, length, angle)
+        else:
+            end_point = point_polygon
+        sd.line(start_point=point, end_point=end_point, color=color, width=3)
+        point = end_point
+
+
+start_point = [(100, 100, 150, 3), (350, 100, 150, 4), (100, 350, 100, 5), (350, 350, 100, 6)]
+
+for _ in start_point:
+    point_start = sd.get_point(_[0], _[1])
+    length_start = _[2]
+    heads_start = _[3]
+    polygon(point_start, heads_start, length_start, color_list_const[_input])
+
 sd.pause()
