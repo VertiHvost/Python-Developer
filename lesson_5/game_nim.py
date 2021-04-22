23794e71e5e642bb5cbff19f8865625e432b6b90
Rufus-02 <vertihvost60@gmail.com>
2021-04-22 14:25:33 +0300

    Сlasswork 5

--- lesson_5/game_nim.py
+++ lesson_5/game_nim.py
@@ -4,6 +4,7 @@ _holder = []
 
 
 def put_stones():
+    '''рассположить камни для начала игры'''
     global _holder
     _holder = []
     for i in range(5):
@@ -11,13 +12,16 @@ def put_stones():
 
 
 def take_from_bunch(position, quantity):
+    """сколько берем из кучки"""
     if 0 <= position <= len(_holder):
         _holder[position - 1] -= quantity
 
 
 def get_bunches():
+    """количество камней в кучке"""
     return _holder
 
 
 def is_gameover():
+    """Условия для выигрыша"""
     return sum(_holder) == 0
--- lesson_5/practice_lesson_5.py
+++ lesson_5/practice_lesson_5.py
@@ -1,11 +1,13 @@
 from game_nim import is_gameover, get_bunches, take_from_bunch, put_stones
+from termcolor import cprint
 
 put_stones()
 user_number = 1
 while True:
-    print('Текущая позиция')
-    print(get_bunches())
-    print('Ход игрока {}'.format(user_number))
+    cprint('Текущая позиция', color='green')
+    cprint(get_bunches(), color='green')
+    user_color = 'blue' if user_number == 1 else 'yellow'
+    cprint('Ход игрока {}'.format(user_number), color=user_color)
     pos = input('Откуда берем?')
     qua = input('Сколько берем?')
     take_from_bunch(position=int(pos), quantity=int(qua))
