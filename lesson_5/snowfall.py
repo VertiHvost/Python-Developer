8391f5fdb45543e3c43fd8b7a4d621efaeac754e
Rufus-02 <vertihvost60@gmail.com>
2021-04-30 00:50:06 +0300

    DZ 5 (v2.4) нихера не получилось пока что

--- lesson_5/snowfall.py
+++ lesson_5/snowfall.py
@@ -3,6 +3,9 @@
 #  сдвинуть_снежинки() - сдвигает снежинки на один шаг
 #  номера_достигших_низа_экрана() - выдает список номеров снежинок, которые вышли за границу экрана
 #  удалить_снежинки(номера) - удаляет снежинки с номерами из списка
+import simple_draw as sd
+
+snowdrops_down = []
 
 
 def list_snowfall(N):
@@ -22,15 +25,21 @@ def draw_snowdrops_with_a_color(point, length, color):
     """Отрисовываем све снежинки цветом 'color' """
     sd.snowflake(center=point, length=length, color=color)
 
+
 def shift_snowdrops(x_list, y_list, length_list, _):
     """ Двигаем снежинки """
     point = sd.get_point(x_list[_], y_list[_])
     point2 = sd.get_point(x_list[_ - 1], y_list[_ - 1])
-    my_snowflake(point2, length_list[_ - 1], color=sd.COLOR_WHITE)
+    draw_snowdrops_with_a_color(point2, length_list[_ - 1], color=sd.COLOR_WHITE)
     y_list[_] -= sd.random_number(1, 4)
     x_list[_] += sd.random_number(-2, 2)
-    my_snowflake(point, length_list[_], color=sd.background_color)
+    draw_snowdrops_with_a_color(point, length_list[_], color=sd.background_color)
 
-def number_of_the_finished_references():
+
+def number_of_the_finished_references(N):
+    global snowdrops_down
+    var = snowdrops_down.append(N)
+    return var
+
+def delete_snowdrops(N):
     pass
-    
