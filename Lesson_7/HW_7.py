9701e70f74640cab93ee0ddfbd75f6510b8c393e
Rufus-02 <vertihvost60@gmail.com>
2021-08-01 22:28:49 +0300

    HW 7.1

--- Lesson_7/HW_7.py
+++ Lesson_7/HW_7.py
@@ -1,22 +1,25 @@
+# -*- coding: utf-8 -*-
+from termcolor import cprint
+
+
 class Road:
 
     def __init__(self, start, end, distance):
-        self.start = start
-        self.end = end
-        self.distance = distance
+        pass
+
 
 class Warehouse:
 
     def __init__(self, name, content=0):
         self.name = name
         self.content = content
-        self.road_out = None
+        self.roed_out = None
 
     def __str__(self):
-        return 'Склад {} груза {}'.format(self.name, self.content)
+        return 'Склад {} груза {} '.format(self.name, self.content)
 
     def set_road_out(self, road):
-        self.road_out = road
+        self.roed_out = road
 
     def track_arrived(self, truck):
         pass
@@ -30,7 +33,8 @@ class Warehouse:
     def act(self):
         pass
 
-class   Vehicle:
+
+class Vehicle:
     fuel_rate = 0
 
     def __init__(self, model):
@@ -43,9 +47,10 @@ class   Vehicle:
     def tank_up(self):
         self.fuel += 1000
 
+
 class Truck(Vehicle):
 
-    def __init__(self, model, body_space = 1000):
+    def __init__(self, model, body_space=1000):
         super().__init__(model=model)
         self.body_space = body_space
         self.cargo = 0
@@ -55,20 +60,20 @@ class Truck(Vehicle):
 
     def __str__(self):
         res = super().__str__()
-        return res + 'груза {}'.format(self.cargo)
-
-    def go_to(self, road):
-        self.place = road
-        print('{} выехал в путь'.format(self.model))
-        self.distance_to_target = road.distance
+        return res + ' груза {}'.format(self.cargo)
 
     def ride(self):
         if self.distance_to_target > self.velocity:
             self.distance_to_target -= self.velocity
-        print('{} выехал в путь, осталось {}'.format(self.model, self.distance_to_target))
+        print('{} едет по дороге, осталось {}'.format(self.model, self.distance_to_target))
+
+    def go_to(self, road):
+        self.place = road
+        self.distance_to_target = road.distance
+        print('{} выехал в путь'.format(self.model))
 
     def act(self):
-        if self.fuel <=10:
+        if self.fuel <= 10:
             self.tank_up()
         elif isinstance(self.place, Road):
             self.ride()
@@ -76,7 +81,7 @@ class Truck(Vehicle):
 
 class AutoLoader(Vehicle):
 
-    def __init__(self, model, bucket_capacity = 100, warehouse = None, role = 'loader'):
+    def __init__(self, model, bucket_capacity=100, warehouse=None, role='loader'):
         super().__init__(model=model)
         self.bucket_capacity = bucket_capacity
         self.warehouse = warehouse
@@ -85,10 +90,10 @@ class AutoLoader(Vehicle):
 
     def __str__(self):
         res = super().__str__()
-        return res + 'груза {}'.format(self.truck)
+        return res + ' грузим {}'.format(self.truck)
 
     def act(self):
-        if self.fuel <=10:
+        if self.fuel <= 10:
             self.tank_up()
         elif self.truck is None:
             self.truck = self.warehouse.get_next_truck()
@@ -98,55 +103,56 @@ class AutoLoader(Vehicle):
             self.unload()
 
     def load(self):
-        truc_cargo_rest = self.truck.body_space - self.truck.cargo
-        if truc_cargo_rest<self.bucket_capacity:
+        target_cargo_rest = self.truck.body_space - self.truck.cargo
+        if self.fuel.cargo >= target_cargo_rest:
             self.warehouse.content -= self.bucket_capacity
             self.truck.cargo += self.bucket_capacity
         else:
-            self.warehouse.content -= truc_cargo_rest
-            self.truck.cargo += truc_cargo_rest
+            self.warehouse.content -= self.bucket_capacity
+            self.truck.cargo += target_cargo_rest
 
     def unload(self):
         if self.truck.cargo >= self.bucket_capacity:
             self.truck.cargo -= self.bucket_capacity
             self.warehouse.content += self.bucket_capacity
         else:
-            self.warehouse.content -= self.truck.cargo
-            self.truck.cargo += self.truck.cargo
+            self.truck.cargo -= self.truck.cargo
+            self.warehouse.content += self.truck.cargo
+
 
 TOTAL_CARGO = 100000
 
-moscow = Warehouse(name='Москва', content= TOTAL_CARGO)
-piter = Warehouse(name='Питер', content = 0)
+moscow = Warehouse(name='Москва', content=TOTAL_CARGO)
+piter = Warehouse(name='Питер', content=0)
 
-moscow_piter = Road(start= moscow, end= piter, distance = 715)
-piter_moscow = Road(start= piter, end=moscow, distance = 780)
+moscow_piter = Road(start=moscow, end=piter, distance=715)
+piter_moscow = Road(start=piter, end=moscow, distance=780)
 
 moscow.set_road_out(moscow_piter)
 piter.set_road_out(piter_moscow)
 
 loader_1 = AutoLoader(model='Bobcat', bucket_capacity=1000, warehouse=moscow, role='loader')
-loader_2 = AutoLoader(model='Lonking', bucket_capacity=500, warehouse=moscow, role='unloader')
+loader_2 = AutoLoader(model='Lonking', bucket_capacity=500, warehouse=piter, role='unloader')
 
-truck_1 = Truck(model='КАМАЗ', body_space='5000')
-truck_2 = Truck(model='ГАЗ', body_space='2000')
+truck_1 = Truck(model='КАМАЗ', body_space=5000)
+truck_2 = Truck(model='ГАЗ', body_space=2000)
 
 moscow.track_arrived(truck_1)
 moscow.track_arrived(truck_2)
 
 hour = 0
 while piter.content < TOTAL_CARGO:
-    hour+=1
-    cprint('----------------------- Час {} -----------------------', format(hour), color = 'red')
+    hour += 1
+    cprint('----------------------- Час {} -----------------------', format(hour), color='red')
     truck_1.act()
     truck_2.act()
     loader_1.act()
     loader_2.act()
     moscow.act()
     piter.act()
-    cprint(truck_1,color='cyan')
-    cprint(truck_2,color='cyan')
-    cprint(loader_1,color='cyan')
-    cprint(loader_2,color='cyan')
-    cprint(moscow,color='cyan')
-    cprint(piter,color='cyan')
+    cprint(truck_1, color='cyan')
+    cprint(truck_2, color='cyan')
+    cprint(loader_1, color='cyan')
+    cprint(loader_2, color='cyan')
+    cprint(moscow, color='cyan')
+    cprint(piter, color='cyan')
